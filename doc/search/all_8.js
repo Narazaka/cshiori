a891e418d59dfc684d4f12e37e8896b58e86d776
Narazaka <info@narazaka.net>
2014-11-11 03:20:32 +0900

    vc++

--- doc/search/all_8.js
+++ doc/search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['readme',['Readme',['../md__readme.html',1,'']]],
   ['reference',['reference',['../structcshiori__request__message.html#a023be362c41b528d1e355748e28f4415',1,'cshiori_request_message::reference()'],['../structcshiori__response__message.html#a317784f7274813d673995f5d50b480f8',1,'cshiori_response_message::reference()']]],
   ['request',['request',['../shiori_8c.html#a0cd1edda97e492ea49b0e0aa1fe67a71',1,'request(const MEMORY_HANDLE h, long *len):&#160;shiori.c'],['../shiori_8h.html#a0cd1edda97e492ea49b0e0aa1fe67a71',1,'request(const MEMORY_HANDLE h, long *len):&#160;shiori.c']]]
 ];
--- doc/shiori_8c.js
+++ doc/shiori_8c.js
@@ -1,7 +1,7 @@
 var shiori_8c =
 [
     [ "SHIORI_LINES_BUFFER_STEP", "shiori_8c.html#a9c809f66b949f254a1bd795d6e4d2efa", null ],
-    [ "load", "shiori_8c.html#ab4372ee350092f56ff55f9642737ccf5", null ],
+    [ "load", "shiori_8c.html#a772f663882073f27e98bd8ade1265592", null ],
     [ "request", "shiori_8c.html#a0cd1edda97e492ea49b0e0aa1fe67a71", null ],
-    [ "unload", "shiori_8c.html#a36a6fa9eefebca2cd43fac8666e2edf4", null ]
+    [ "unload", "shiori_8c.html#a29c9546a8002aee4ab639a72a5756a6a", null ]
 ];
